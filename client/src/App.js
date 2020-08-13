f0ffdec69ff72481af27baf5e8018244e0ee4f1d
Dennis <dennissarmiento808@gmail.com>
2020-08-12 20:13:26 -0400

    up

---
+++ client/src/App.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    title: '',
+    body: '',
+    posts:[]
+  };
+
+  // handleChange = (event)=>{
+  //   const target = event.target;
+  //   const name = target.name;
+  //   const value = target.value;
+  //   this.setState({
+  //     [name]:value
+  //   });
+  // }
+
+  componentDidMount =()=>{
+    this.getBlogPost();
+  }
+  handleChange = ({target})=>{
+    const {name, value} = target;
+    this.setState({ [name]:value });
+  }
+
+  submitPost = (event) => {
+    event.preventDefault();
+    if(this.state.title.trim() === ""){
+      alert("please fill out the blanks")
+    } else if(this.state.body.trim()=== ""){
+      alert("please fill out the body")
+    } else {
+      const payload = {
+        title: this.state.title,
+        body: this.state.body
+      };
+      console.log(payload);
+      this.sendBlogPost(payload);
+    }
+  }
+
+  sendBlogPost = (data) => {
+    axios({
+      url:'/api/save',
+      method: 'POST',
+      data: data
+    })
+      .then((response)=>{
+        console.log("Data sent to the server: ", response);
+        this.resetUserInputs();
+        this.getBlogPost();
+      })
+      .catch((error)=>{
+        console.log("data not sent to the server:" + error)
+      });
+  }
+
+  resetUserInputs = ()=>{
+    this.setState({
+      title: '',
+      body:''
+    })
+  }
+
+  getBlogPost = () => {
+    axios.get('/api')
+      .then((response)=>{
+        const data = response.data.reverse();
+        console.log(data);
+        this.setState({
+          posts: data
+        })
+        console.log("data received", this.state.posts);
+      })
+      .catch((error)=>{
+        console.log(error)
+      });
+  }
+
+  displayBlogPost = (posts) => {
+    if(!posts.length) {
+      return null
+    } else {
+       return posts.map((post, index)=>{
+         return (
+           <div key={post._id} className="blog-post-display">
+             <h3>{post.title}</h3>
+             <p>{post.body}</p>
+           </div>
+         )
+       })
+    }
+  }
+
+  render(){
+    //use comma to show the object if you use + it will not show the object
+    console.log("STATE: ", this.state);
+    return (
+
+      <div className="app"> 
+        <h2>Welcome, Post Something!</h2>
+        <form onSubmit={this.submitPost}>
+          <div className="form-input">  
+            <input 
+            type="text"
+            name="title"
+            placeholder="enter title"
+            value={this.state.title}
+            onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-input">
+            <textarea 
+            placeholder="body" 
+            name="body" 
+            cols="30" 
+            rows="10" 
+            value={this.state.body} 
+            onChange={this.handleChange}
+            >
+
+            </textarea>
+
+          </div>
+          <button> Submit</button>
+
+        </form>
+        <div className="blog-post">
+          {this.displayBlogPost(this.state.posts)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+// const cors = require('cors');
+const path = require('path');
+const app = express();
+
+//the process.env.PORT will be used by heroku
+//whatever port heroku can give it will give you by using procerss.env.PORT
+//to refresh the terminal type: killall -9 node
+const PORT = process.env.PORT || 8080;
+// const PORT = 8000;
+//go in the routes folder and use the FILE in particular
+const routes = require('./routes/api')
+const BlogPost = require('./models/blogPost');
+
+// const MONGODB_URI = 'mongodb+srv://88dennis:88dennis@cluster0.m6gfk.gcp.mongodb.net/mypostapp?retryWrites=true&w=majority'
+
+// || 'mongodb://localhost:27017/mypostapp'
+// mongoose.connect('mongodb://localhost:27017/mypostapp', {
+// mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/mypostapp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mypostapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//this will tell you if you are connected
+mongoose.connection.on('connected', ()=>{
+    console.log('Mongoose is connected')
+});
+
+// app.use(bodyParser.json());
+app.use(express.json());
+//for simple objects you may use extended false
+//for deep nested objects use true
+app.use(express.urlencoded({extended: false}));
+
+
+
+//==========================================
+//SAVING SOMETHING IN DATABASE - FAKE DATA
+// app.get("/seed", function(req, res){
+//     const data = {
+//         title: 'Welcome to my post',
+//         body: 'This is my first Post...........',
+    
+//     };
+//     //creating a new instance of your model
+//     //this instance is where we pass the data
+//     const newBlogPost = new BlogPost(data);
+//     console.log(newBlogPost)
+//     //.save() the new instance
+//     // newBlogPost.save((error)=>{
+//     //     if(error){
+//     //         console.log('Error saving the data');
+//     //     } else {
+//     //         console.log('Data has been saved');
+//     //     }
+//     // })
+//     res.json(newBlogPost)
+// });
+//=================================
+
+
+
+
+//cors helps you access servers from different origins
+//you can use proxy too for a way around it
+//in package.json client folder insert proxy with the port you are using in your backend
+//the PROXY will let client search for any routes in your server port if it is not on 3000
+// app.use(cors());
+
+//morgan is HTTP request logger - will log what routes we are hitting
+app.use(morgan('tiny'));
+
+//CONFIGURE THE ROUTES
+//define a starting point = "/" or "/api" or anything
+// app.use("/", routes)
+app.use("/api", routes) 
+
+if(process.env.NODE_ENV === 'production') {
+    //get to the build folder
+    app.use(express.static('client/build'));
+}
+
+
+app.listen(PORT, () => {
+    console.log(`server connected to ${PORT}`)
+});
